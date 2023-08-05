00783cb77e56aeae9eb92535b328dc08314e6d67
Abdulrahman Abubaker <102293968+rahmoondev@users.noreply.github.com>
2023-08-05 18:35:11 -0400

    my change

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
-topics= list([
-        {"id":1,"title":"Topic 1", "body":"slkjs skjskjs skjsks kjs"},
-        {"id":2,"title":"Topic 2", "body":"slkjs skjskjs skjsks kjs"},
-        {"id":3,"title":"Topic 3", "body":"slkjs skjskjs skjsks kjs"}
-    ])
+import tkinter as tk
 
+root = tk.Tk()
 
-idx=0
-for one in topics:
-    if one["id"]==2:
-        topics.remove(one)
-        topics.insert(idx, {"id":2,"title":"aaaaaaaaTopic 2", "body":"aaaaaaaaslkjs skjskjs skjsks kjs"})
-        break
-    idx=idx+1
+root.geometry("800x500")
 
-print(topics)
+root.title("MY FIRST GUI")
+
+label = tk.Label(root,text="hello world",font=('Arial',22)) 
+
+label.pack()
+root.mainloop()
